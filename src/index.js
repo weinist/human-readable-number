805d5c1a4ce9082ad1d12466a8225eec892287f0
Daria Trubnikova <daria.trubnikova@gmail.com>
2021-03-14 22:42:04 +0300

    fix: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function toReadable (number) {
-  
+  const numText = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+  'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+  const dozenText = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+
+  let hundredText = '';
+
+  if(number === 0) {
+    return 'zero';
+  }
+
+  const hundreds = Math.floor(number / 100);
+
+  let dozens = Math.floor((number - hundreds * 100) / 10);
+  let digits = Math.floor(number - hundreds * 100 - dozens * 10);
+
+  if(dozens === 1) {
+    dozens = 0;
+    digits = number - hundreds * 100;
+  }
+
+  if((hundreds !== undefined) && (hundreds > 0)) {
+    hundredText = numText[hundreds] + ' hundred ';
+  }
+  return (hundredText + dozenText[dozens] + numText[digits]).trim()
 }
